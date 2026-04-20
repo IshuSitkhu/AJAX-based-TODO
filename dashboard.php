68ced06b3ce19db80392de57bc1481ef7065974e
unknown <ishusitikhu6@gmail.com>
2026-04-20 15:19:15 +0545

    Implemented login system

---
+++ dashboard.php
@@ -0,0 +1,31 @@
+<?php
+include 'auth.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+
+<body>
+
+<h2>Welcome <?php echo $_SESSION['name']; ?></h2>
+
+<?php if ($_SESSION['role'] == 'admin'): ?>
+    <h3>Admin Panel</h3>
+    <a href="create_user.html">Create User</a>
+<?php else: ?>
+    <h3>Staff Panel</h3>
+<?php endif; ?>
+
+<br><br>
+<a href="logout.php">Logout</a>
+
+<hr>
+
+<!-- Include your existing todo UI -->
+<iframe src="index.html" width="500" height="500"></iframe>
+
+</body>
+</html>
