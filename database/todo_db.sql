ae79f02d3e18573f3e2bcf612754da2b336cb7dc
unknown <ishusitikhu6@gmail.com>
2026-04-23 17:20:37 +0545

    Added database schema

---
+++ database/todo_db.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2026 at 01:32 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `todo_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
+--
+
+CREATE TABLE `todos` (
+  `id` int(11) NOT NULL,
+  `task` varchar(255) DEFAULT NULL,
+  `status` enum('pending','completed') DEFAULT 'pending',
+  `user_id` int(11) DEFAULT NULL,
+  `assigned_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `todos`
+--
+
+INSERT INTO `todos` (`id`, `task`, `status`, `user_id`, `assigned_by`) VALUES
+(12, 'HOMEWORK', 'pending', NULL, NULL),
+(15, 'HAndle Social Media', 'pending', 6, 1),
+(17, 'abc', 'pending', 4, 1),
+(20, 'php', 'pending', 8, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `role` enum('admin','staff') DEFAULT 'staff'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'Admin', 'admin@gmail.com', '$2y$10$K5QjSosxnaNlmz6GESnqP.KGVvN3OlM03H0R4fz0rvjuBRY1/vnfm', 'admin'),
+(3, 'Test User1', 'test1@gmail.com', '$2y$10$QmpGoKNx.FpttqBcyGDDlOQXA4bpCR9xcZew8GAC38P1T5IJq05IC', 'staff'),
+(4, 'krishna', 'krishna@gmaill.com', '$2y$10$qg0eh28PyRxerzZSyzyUgOe4uUYRO6kZInkhVz01Z/1j3rHiBHwGK', 'staff'),
+(6, 'ram', 'ram@gmail.com', '$2y$10$0LlyaZjiieuxvck7kPoCzuqERD1XIIxbbF5f6.Zl/rEmWMqYRuCgC', 'staff'),
+(7, 'abc', 'abc@gmail.com', '$2y$10$DlREsgLtCXzJDrbOyn2treeABMw4KKvOK9iTzqDg6vMrDdeGk1kSO', 'staff'),
+(8, 'Ishu', 'ishusitikhu6@gmail.com', '$2y$10$xtq4HD5soGvjbVQETgm7Gu6S2XRa7qUZNlOeu5.sYqaBa1VdwYfx6', 'staff'),
+(23, 'Ishu', 'ishusitikhu1@gmail.com', '$2y$10$Iv2GOkLYfT//sbvLXnu43OCIhpQ/.n7EeeP.xjkFgLhcXbTqeTflW', 'staff');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `todos`
+--
+ALTER TABLE `todos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `todos`
+--
+ALTER TABLE `todos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
