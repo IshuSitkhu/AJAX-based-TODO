8cb07bfb92d0ab5e8c366d7d384b3ebd4f4a2a92
unknown <ishusitikhu6@gmail.com>
2026-04-28 13:53:24 +0545

    update database is exported

--- database/todo_db.sql
+++ database/todo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2026 at 01:44 PM
+-- Generation Time: Apr 28, 2026 at 10:07 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,9 +29,42 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `projects` (
   `id` int(11) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `description` text DEFAULT NULL
+  `title` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `projects`
+--
+
+INSERT INTO `projects` (`id`, `title`, `description`, `created_at`) VALUES
+(38, 'TODO Project', 'helloo', '2026-04-28 07:22:19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project_tasks`
+--
+
+CREATE TABLE `project_tasks` (
+  `id` int(11) NOT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `task` varchar(255) DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'pending',
+  `assigned_user_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `assigned_by` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `project_tasks`
+--
+
+INSERT INTO `project_tasks` (`id`, `project_id`, `task`, `status`, `assigned_user_id`, `created_at`, `assigned_by`) VALUES
+(11, 38, 'Database', 'pending', 8, '2026-04-28 07:22:48', NULL),
+(13, 38, 'PHP LARAVEL', 'pending', 8, '2026-04-28 07:45:00', NULL),
+(15, 38, 'Ui', 'pending', 25, '2026-04-28 07:52:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -43,6 +76,26 @@ CREATE TABLE `project_users` (
   `id` int(11) NOT NULL,
   `project_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `project_users`
+--
+
+INSERT INTO `project_users` (`id`, `project_id`, `user_id`) VALUES
+(21, 38, 25),
+(22, 38, 8);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `task_users`
+--
+
+CREATE TABLE `task_users` (
+  `id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -67,7 +120,8 @@ INSERT INTO `todos` (`id`, `task`, `status`, `user_id`, `assigned_by`) VALUES
 (12, 'HOMEWORK', 'pending', NULL, NULL),
 (15, 'HAndle Social Media', 'pending', 6, 1),
 (17, 'abc', 'pending', 4, 1),
-(20, 'php', 'pending', 8, 1);
+(20, 'php', 'pending', 8, 1),
+(21, 'php laravel', 'pending', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -89,12 +143,11 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
 (1, 'Admin', 'admin@gmail.com', '$2y$10$K5QjSosxnaNlmz6GESnqP.KGVvN3OlM03H0R4fz0rvjuBRY1/vnfm', 'admin'),
-(3, 'Test User1', 'test1@gmail.com', '$2y$10$QmpGoKNx.FpttqBcyGDDlOQXA4bpCR9xcZew8GAC38P1T5IJq05IC', 'staff'),
-(4, 'krishna', 'krishna@gmaill.com', '$2y$10$qg0eh28PyRxerzZSyzyUgOe4uUYRO6kZInkhVz01Z/1j3rHiBHwGK', 'staff'),
+(3, 'Test1', 'test1@gmail.com', '$2y$10$QmpGoKNx.FpttqBcyGDDlOQXA4bpCR9xcZew8GAC38P1T5IJq05IC', 'staff'),
 (6, 'ram', 'ram@gmail.com', '$2y$10$0LlyaZjiieuxvck7kPoCzuqERD1XIIxbbF5f6.Zl/rEmWMqYRuCgC', 'staff'),
 (7, 'abc', 'abc@gmail.com', '$2y$10$DlREsgLtCXzJDrbOyn2treeABMw4KKvOK9iTzqDg6vMrDdeGk1kSO', 'staff'),
 (8, 'Ishu', 'ishusitikhu6@gmail.com', '$2y$10$xtq4HD5soGvjbVQETgm7Gu6S2XRa7qUZNlOeu5.sYqaBa1VdwYfx6', 'staff'),
-(23, 'Ishu', 'ishusitikhu1@gmail.com', '$2y$10$Iv2GOkLYfT//sbvLXnu43OCIhpQ/.n7EeeP.xjkFgLhcXbTqeTflW', 'staff');
+(25, 'Krishna', 'Krishna@gmail.com', '$2y$10$1sLBqBR8PnCFnbUL2cv59O9a2S/sMon9LoDqHbRSsGHAHw575oPw2', 'staff');
 
 --
 -- Indexes for dumped tables
@@ -107,10 +160,24 @@ ALTER TABLE `projects`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `project_tasks`
+--
+ALTER TABLE `project_tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `project_users`
 --
 ALTER TABLE `project_users`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `task_users`
+--
+ALTER TABLE `task_users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `task_id` (`task_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `todos`
@@ -133,25 +200,48 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+
+--
+-- AUTO_INCREMENT for table `project_tasks`
+--
+ALTER TABLE `project_tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `project_users`
 --
 ALTER TABLE `project_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `task_users`
+--
+ALTER TABLE `task_users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `task_users`
+--
+ALTER TABLE `task_users`
+  ADD CONSTRAINT `task_users_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `project_tasks` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `task_users_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
